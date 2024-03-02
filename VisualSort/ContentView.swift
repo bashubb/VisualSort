d2495f0b4cccd345c4953e112cd56033c5e2267d
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2024-03-02 22:14:15 +0100

    Create Animated Card for sorting Info

--- VisualSort/ContentView.swift
+++ VisualSort/ContentView.swift
@@ -14,81 +14,116 @@ struct ContentView: View {
         case quick = "Quick Sort"
     }
     
-    
     @State private var values = (1...100).map(SortValue.init).shuffled()
-    
     @State private var timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
-    
     @State private var timerSpeed = 0.1
-    
     @State private var sortPosition = 1
-    
     @State private var sortFunction = SortTypes.bubble
-    
     @State private var animationSpeed = 1.0
     
+    @State private var expandMoreInfo = false
+    @State private var showInfo = false
+    
     var body: some View {
-        VStack(spacing:20) {
-            Text("VisualSort")
-                .font(.largeTitle)
-                .fontWeight(.semibold)
-            
-            GeometryReader { proxy in
-                HStack(spacing: 0) {
-                    ForEach(values) { value in
-                        Rectangle()
-                            .fill(value.color)
-                            .frame(width: proxy.size.width * 0.01, height: proxy.size.height * Double(value.id) / 100)
+        ZStack {
+            VStack(spacing:20) {
+                Text("VisualSort")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                
+                GeometryReader { proxy in
+                    HStack(spacing: 0) {
+                        ForEach(values) { value in
+                            Rectangle()
+                                .fill(value.color)
+                                .frame(width: proxy.size.width * 0.01, height: proxy.size.height * Double(value.id) / 100)
+                        }
+                    }
+                }
+                .padding(.bottom)
+                
+                Picker("Sort Type", selection: $sortFunction) {
+                    ForEach(SortTypes.allCases, id: \.self) {
+                        Text($0.rawValue)
+                    }
+                }
+                .pickerStyle(.segmented)
+                
+                HStack(spacing: 20) {
+                    LabeledContent("Delay") {
+                        Slider(value: $timerSpeed, in: 0.01...1)
+                    }
+                    
+                    Button("Step", action: step)
+                    Button("Shuffle") {
+                        withAnimation {
+                            values.shuffle()
+                            sortPosition = 1
+                        }
                     }
                 }
             }
-            .padding(.bottom)
-            
-            Picker("Sort Type", selection: $sortFunction) {
-                ForEach(SortTypes.allCases, id: \.self) {
-                    Text($0.rawValue)
+            .preferredColorScheme(.dark)
+            .padding()
+            .onReceive(timer) { _ in
+                step()
+            }
+            .onChange(of: timerSpeed) {
+                if timerSpeed == 1 || sortFunction.rawValue == "Quick Sort"{
+                    timer.upstream.connect().cancel()
+                } else {
+                    timer = Timer.publish(every: timerSpeed, on: .main, in: .common).autoconnect()
                 }
             }
-            .pickerStyle(.segmented)
-            
-            
-            HStack(spacing: 20) {
-                LabeledContent("Delay") {
-                    Slider(value: $timerSpeed, in: 0.01...1)
+            .onChange(of: sortFunction) {
+                sortPosition = 1
+                if sortFunction.rawValue == "Quick Sort"{
+                    animationSpeed = 0.2
+                } else {
+                    animationSpeed = 1
                 }
-                
-                Button("Step", action: step)
-                
-                Button("Shuffle") {
+            }
+            
+            // Info Card
+            GeometryReader { gp in
+                VStack(spacing: 15) {
+                    Spacer()
+                    VStack(spacing: 10) {
+                        Text("More info about sorting here")
+                        Spacer()
+                    }
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(RoundedRectangle(cornerRadius: 15).opacity(0.2))
+                    .padding(.horizontal)
+                    .opacity(showInfo ? 1 : 0)
+                    
+                    HStack {
+                        Spacer()
+                        Text("More info")
+                        Image(systemName: "info.circle")
+                            .padding(.horizontal)
+                    }
+                    .font(.title)
+                }
+                .padding(.bottom, 3)
+                .background(RoundedRectangle(cornerRadius:20).fill(Color.yellow).shadow(radius: 8))
+                .foregroundStyle(.black)
+                .offset(x: expandMoreInfo ? -15 : -gp.size.width + 15,
+                        y: expandMoreInfo ? -15 : -gp.size.height + 40)
+                .onTapGesture{
                     withAnimation {
-                        values.shuffle()
-                        sortPosition = 1
+                        expandMoreInfo.toggle()
+                    }
+                    withAnimation(.default.delay(0.5)) {
+                        showInfo.toggle()
                     }
                 }
-                
-            }
-            
-            
-        }
-        .preferredColorScheme(.dark)
-        .padding()
-        .onReceive(timer) { _ in
-            step()
-        }
-        .onChange(of: timerSpeed) {
-            if timerSpeed == 1 || sortFunction.rawValue == "Quick Sort"{
-                timer.upstream.connect().cancel()
-            } else {
-                timer = Timer.publish(every: timerSpeed, on: .main, in: .common).autoconnect()
             }
         }
-        .onChange(of: sortFunction) {
-            sortPosition = 1
-            if sortFunction.rawValue == "Quick Sort"{
-                animationSpeed = 0.2
-            }
-        }
+        
     }
+    
     func step() {
         withAnimation(.easeInOut.speed(animationSpeed)) {
             switch sortFunction {
@@ -101,6 +136,7 @@ struct ContentView: View {
             }
         }
     }
+    
 }
 
 #Preview {
